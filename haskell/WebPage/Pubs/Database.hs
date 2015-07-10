e5f1e2560853bf4a57de6d6002a826f9542b086c
abbottk <abbottk@eecs.oregonstate.edu>
2015-07-10 12:45:53 -0700

    pubs database update

--- haskell/WebPage/Pubs/Database.hs
+++ haskell/WebPage/Pubs/Database.hs
@@ -10,7 +10,7 @@ import WebPage.Pubs.Paper
 --
 
 -- ** Authors
-abbottk     = Author "Keeley" "Abbott"
+abbott      = Author "Keeley" "Abbott"
 walkingshaw = Author "Eric" "Walkingshaw"
 bogart      = Author "Christopher" "Bogart"
 
@@ -20,15 +20,30 @@ osu = "Oregon State University"
 
 
 -- ** Journals
--- jfp    = short "JFP" "Journal of Functional Programming"
+jfp    = short "JFP" "Journal of Functional Programming"
+jvlc   = short "JVLC" "Journal of Visual Languages and Computing"
+toplas = short "TOPLAS" "ACM Trans. on Programming Languages and Systems"
+tosem  = short "TOSEM" "ACM Trans. on Software Engineering and Methodology"
 
 
 -- ** Conferences
--- lncs = ("LNCS",)
+lncs = ("LNCS",)
+
+dsl    = short "DSL" "IFIP Working Conf. on Domain-Specific Languages"
+idetc  = short "IDETC" "ASME Int. Design Engineering Technical Conf. & Computers and Information in Engineering Conf."
+gpce   = short "GPCE" "ACM SIGPLAN Int. Conf. on Generative Programming and Component Engineering"
+gttse  = short "GTTSE" "Generative and Transformational Techniques in Software Engineering"
+icfp   = short "ICFP" "ACM SIGPLAN Int. Conf. on Functional Programming"
+onward = short "Onward!" "ACM SIGPLAN Symp. on New Ideas in Programming and Reflections on Software"
+sle    = short "SLE" "Int. Conf. on Software Language Engineering"
+vlhcc  = short "VL/HCC" "IEEE Symposium on Visual Languages and Human-Centric Computing"
+vlhccgc | Just s <- _shortname vlhcc = short s ("Graduate Consortium at " ++ _longname vlhcc)
 
 
 -- ** Workshops
--- fosd  = short "FOSD" "Int. Workshop on Feature-Oriented Software Development"
+fosd  = short "FOSD" "Int. Workshop on Feature-Oriented Software Development"
+foser = short "FoSER" "ACM SIGSOFT Workshop on the Future of Software Engineering Research"
+vamos = short "VaMoS" "Int. Workshop on Variability Modelling of Software-Intensive Systems"
 
 
 --
@@ -38,16 +53,16 @@ osu = "Oregon State University"
 -- ** Lists of papers in chronological order.
 
 drafts = []
-y15 = []
+y15 = [vlhcc15]
 
 allPubs = concat [drafts,y15]
 
 
--- ** 2014
+-- ** 2015
 
--- onward14 = accepted Conference
---   "onward14-variational-data"
---   [walkingshaw,kaestner,erwig,apel,bodden]
---   "Variational Data Structures: Exploring Trade-Offs in Computing With Variability"
---   2014
---   @@ onward
+vlhcc15 = accepted Conference
+  "programs-for-people"
+  [abbott,bogart,walkingshaw]
+  "Programs for People: What We Can Learn from Lab Protocols"
+  2015
+  @@ vlhcc
